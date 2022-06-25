37618339af95c49052ca62cb699d719d8db4745e
SebastienFoly <Sebastien.Foly@Student.HTW-Berlin.de>
2022-06-25 18:12:53 +0200

    Soweit alle Attribute + 2 diff. Construct +set/get

--- model/Vampire.java
+++ model/Vampire.java
@@ -1,8 +1,177 @@
 package model;
+
+import java.util.UUID;
+
 public class Vampire {
  
-    private String id;
-    private int age;
+    private String id=UUID.randomUUID().toString();
+    private String name;
+    private int grandness=0;
+    private int hunger=5;
+    private CreatorVampire creator;
+    private boolean isDrinkingBlood;
+    private boolean inFight;
+    private boolean canControllInstincts;
+    private int energy=10;
+    private boolean finallyDead;
+    
+
+    public Vampire(String name, CreatorVampire creator){
+
+        this.name = name;
+        this.creator = creator;
+
+    }
+    
+    
+    public Vampire(String id, String name, int grandness, int hunger, CreatorVampire creator, boolean isDrinkingBlood,
+            boolean inFight, boolean canControllInstincts, int energy, boolean finallyDead) {
+        this.id = id;
+        this.name = name;
+        this.grandness = grandness;
+        this.hunger = hunger;
+        this.creator = creator;
+        this.isDrinkingBlood = isDrinkingBlood;
+        this.inFight = inFight;
+        this.canControllInstincts = canControllInstincts;
+        this.energy = energy;
+        this.finallyDead = finallyDead;
+    }
+
+
+
+    
+
+
+
+    public String getId() {
+        return id;
+    }
+
+
+
+
+
+
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+
+
+
+
+
+
+    public String getName() {
+        return name;
+    }
+
+
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+
+    public int getGrandness() {
+        return grandness;
+    }
+
+
+
+    public void setGrandness(int grandness) {
+        this.grandness = grandness;
+    }
+
+
+
+    public int getHunger() {
+        return hunger;
+    }
+
+
+
+    public void setHunger(int hunger) {
+        this.hunger = hunger;
+    }
+
+
+
+    public CreatorVampire getCreator() {
+        return creator;
+    }
+
+
+
+    public void setCreator(CreatorVampire creator) {
+        this.creator = creator;
+    }
+
+
+
+    public boolean isDrinkingBlood() {
+        return isDrinkingBlood;
+    }
+
+
+
+    public void setDrinkingBlood(boolean isDrinkingBlood) {
+        this.isDrinkingBlood = isDrinkingBlood;
+    }
+
+
+
+    public boolean isInFight() {
+        return inFight;
+    }
+
+
+
+    public void setInFight(boolean inFight) {
+        this.inFight = inFight;
+    }
+
+
+
+    public boolean isCanControllInstincts() {
+        return canControllInstincts;
+    }
+
+
+
+    public void setCanControllInstincts(boolean canControllInstincts) {
+        this.canControllInstincts = canControllInstincts;
+    }
+
+
+
+    public int getEnergy() {
+        return energy;
+    }
+
+
+
+    public void setEnergy(int energy) {
+        this.energy = energy;
+    }
+
+
+
+    public boolean isFinallyDead() {
+        return finallyDead;
+    }
+
+
+
+    public void setFinallyDead(boolean finallyDead) {
+        this.finallyDead = finallyDead;
+    }
+
+
+    
     
 
 
