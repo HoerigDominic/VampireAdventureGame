14047e813a7cf3a7602870bb074d25234b3237a8
HoerigDominic <s0578252@htw-berlin.de>
2022-06-28 22:26:03 +0200

    Konstruktor erstellt

--- model/CreatorVampire.java
+++ model/CreatorVampire.java
@@ -1,5 +1,12 @@
 package model;
 
-public class CreatorVampire {
+public class CreatorVampire extends Vampire {
+
+    private int descendants = 0;
+
+    public CreatorVampire(String name) {
+        super (name);
+        this.descendants = 0; 
+    } 
 
 }
