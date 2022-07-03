e950b097c2a40af16fdca5b8ca8d3408baf66ebf
HoerigDominic <s0578252@htw-berlin.de>
2022-07-03 21:38:49 +0200

    Methoden teilweise implementiert

--- model/Human.java
+++ model/Human.java
@@ -1,6 +1,39 @@
 package model;
 
+import java.util.UUID;
+import java.util.Random;
+
 public class Human {
     
-    private String name;
+    private String id = UUID.randomUUID().toString();
+    Random amountOfBlood = new Random();
+    double amountOfBloodValue = amountOfBlood.nextDouble()*8;
+
+
+    public Human () {
+
+        this.amountOfBlood = amountOfBlood;
+        this.id = UUID.randomUUID().toString();
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public int getAmountOfBlood() {
+        return amountOfBlood;
+    }
+
+    public void setAmountOfBlood(int amountOfBlood) {
+        this.amountOfBlood = amountOfBlood;
+    }
+
+    
+
+
+
 }
