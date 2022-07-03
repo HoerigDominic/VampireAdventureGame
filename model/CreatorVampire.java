43935f4cd7b739737cf7618f4d619e3fe244ca28
HoerigDominic <s0578252@htw-berlin.de>
2022-07-03 21:45:53 +0200

    Methoden ergänzt

--- model/CreatorVampire.java
+++ model/CreatorVampire.java
@@ -17,7 +17,42 @@ public class CreatorVampire extends Vampire {
         this.descendants = descendants;
     }
     
-    
+    //Operation
+    //listAllDescendants
+    public void listAllDescendants() {
 
+        System.out.println("" + this.getDescendants());
+    }
+
+    //commandToDrinkBlood
+    public void commandToDrinkBlood(Vampire vampire, int amount) {
+
+        vampire.drinkBlood(amount);
+        System.out.println(vampire.getName() + " drunk " + amount + "L of the human blood!");
+
+        // Verbindung mit Human to Vampire 
+    }
+
+    //commandToAttack
+    public void commandToAttack(Vampire vampire) {
+
+        // Human ????
+        vampire.attackHuman(Human);
+    }
+
+    //sacrifice
+    public void sacrifice(Vampire vampire) {
+
+        vampire.setEnergy(0);
+        vampire.setFinallyDead(true);
+        //Wie kann Vampir gesperrt werden dann ???! 
+        /*Der Vampirjäger löscht den geopferten Vampir aus und lässt
+        den CreatorVampir mit einer Wahrscheinlichkeit von 50% in Frieden. Mit einer
+        Wahrscheinlichkeit von 50% greift er ihn jedoch weiter an. */
+        if ()
+    }
+
+    //flee
     
 }
+
