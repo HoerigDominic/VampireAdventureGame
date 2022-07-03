a9f2c75203e2b44f63cce838027a11efe34d8480
HoerigDominic <s0578252@htw-berlin.de>
2022-07-03 21:47:23 +0200

    Mthoden ergänzt,Zufallsblutmenge

--- model/Human.java
+++ model/Human.java
@@ -2,18 +2,18 @@ package model;
 
 import java.util.UUID;
 import java.util.Random;
+-
 
 public class Human {
     
     private String id = UUID.randomUUID().toString();
-    Random amountOfBlood = new Random();
-    double amountOfBloodValue = amountOfBlood.nextDouble()*8;
+    private double amountOfBlood = generateRandom();
 
-
-    public Human () {
+    public Human (double amountOfBlood) {
 
         this.amountOfBlood = amountOfBlood;
         this.id = UUID.randomUUID().toString();
+
     }
 
     public String getId() {
@@ -24,16 +24,35 @@ public class Human {
         this.id = id;
     }
 
-    public int getAmountOfBlood() {
-        return amountOfBlood;
+    //defend
+
+    //flee
+
+    //turnIntoVampire
+    public void turnIntoVampire(Vampire vampire) {
+
+        if (this.amountOfBlood < 5) {
+            Vampire humanToVampire = new Vampire();
+            vampire.getName();
+            humanToVampire.setCreator();
+
+            //Creator setzen für den neuen Vampir ?
+
+            
+        }
     }
 
-    public void setAmountOfBlood(int amountOfBlood) {
-        this.amountOfBlood = amountOfBlood;
+    // Definiert Blutmenge zwischen 6-8Liter als Double 
+    public int generateRandom() {
+
+    Random randomNumber = new Random();
+    int randomNumberValue = randomNumber.nextInt(8);
+
+    while (randomNumberValue < 6 ) {
+        randomNumberValue = randomNumber.nextInt(8);
+    } 
+    return randomNumberValue;
     }
-
-    
-
 
 
 }
