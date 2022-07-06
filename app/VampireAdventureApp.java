ccd28aad628d07445243ed9e25920e588bd4c19d
HoerigDominic <s0578252@htw-berlin.de>
2022-07-06 15:26:38 +0200

    Aufgabe 6d fertig

--- app/VampireAdventureApp.java
+++ app/VampireAdventureApp.java
@@ -4,6 +4,8 @@ import java.util.Scanner;
         
 import model.CreatorVampire;
 import model.Vampire;
+import model.Human;
+import model.VampireHunter;
 
 /**
  * @author vemaj
@@ -24,7 +26,8 @@ public class VampireAdventureApp {
             handle(choice);
             System.out.println("============");
         }           // Schleife aktiviert @Team
-    }
+        
+    }      
 
     /**
      * 
@@ -54,15 +57,19 @@ public class VampireAdventureApp {
                 createVampire();
                 break;
             case 2:
+                showSelectedVampire();
                 break;
             case 3:
+                listAllVampires();
                 break;
             case 4:
+                deletVampire();
                 break;
             case 5:
+                startNighltyAdventure();
                 break;
             case 6:
-                quitGame(); // Funktion Team
+                quitGame(); 
                 break;
             default: {
                 System.out.println("Invalid input. Please choose a correct number between 1 and 6");
@@ -79,7 +86,7 @@ public class VampireAdventureApp {
     private static void showMenu() {
 
         String[] menuItems = { "", "(1)\t Create Vampire", "(2)\t Show Selected Vampire", "(3)\t List all Vampire", "(4)\t Delete Vampire", "(5)\t Start Nightly Adventure", "(6)\t Quit"};
-        //Menupunkte Team
+        
         System.out.println("\nVampire Adventures 1.0\n");
         for (int i = 1; i < menuItems.length; i++) {
             System.out.println(menuItems[i]);
@@ -87,31 +94,55 @@ public class VampireAdventureApp {
 
     }
 
-
-    //
+    //createVampire
     private static void createVampire() {
 
-        CreatorVampire creatorRandom = new CreatorVampire("");
-        System.out.println("Please enter a Name for your Creator Vampire!");
-        creatorRandom.setName(System.in);
+        System.out.print("\nPlease enter a Name for your Creator Vampire: ");
+        CreatorVampire player = new CreatorVampire(scanner.next());
 
-        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-        String input = in.readLine();
+        System.out.print("\nPlease enter a Name for first Descandent: ");
+        Vampire vampire1 = new Vampire(scanner.next(), player);
+
+        System.out.print("\nPlease enter a Name for second Descandent: ");
+        Vampire vampire2 = new Vampire(scanner.next(), player);
         
-        Vampire vampireRandom1 = new Vampire("Vampire1",creatorRandom);
-        Vampire vampireRandom2 = new Vampire("Vampire2",creatorRandom);
-
-
+        Vampire[] team = new Vampire [3];
+        team[0] = player; 
+        team[1] = vampire1;
+        team[2] = vampire2;
+        System.out.println("\nWell done "+player.getName()+"! \nTogether with your two descendents you can start a Nightly Adventure!\n");
         
+    } 
+
+    //showSelectedVampire
+    private static void showSelectedVampire() {
+
+        System.out.print("\nPlease select a Vampire to show the stats: ");
+    
+        scanner.next();
+
+    }
+    
+    //listAllVampires
+    public static void listAllVampires() {
+
     }
 
-    // Team
+    //deleteVampire
+    public static void deletVampire() {
+
+    }
+
+    //startNighltyAdventure
+    public static void startNighltyAdventure() {
+
+    }
+
+    //quit
     private static void quitGame() {
         System.out.println("\nThe Game has been closed!\n");
         System.exit(0);
 
     }
-
-
-    
+  
 }
